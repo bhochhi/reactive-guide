97593b5c7ca836caa6de6190609646d07be565a1
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2016-02-18 11:14:55 -0600

    simple reactive java app using rxjava

---
+++ examples/rxjava-simple-app/src/main/java/com/codewithe/bhochhi/App.java
@@ -0,0 +1,28 @@
+package com.codewithe.bhochhi;
+
+import rx.Observable;
+import rx.functions.Action1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        sayHello("Xing","Sameeri","Me");
+    }
+    
+    
+    
+    public static void sayHello(String... names) {
+    	Observable.from(names).subscribe(new Action1<String>() {
+			public void call(String name) {
+				System.out.println("Hello "+name+"!");
+				
+			}
+		
+    	});
+    }
+}
